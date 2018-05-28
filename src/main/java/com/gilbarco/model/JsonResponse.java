cb0b980bc581293f6e55725734cb2ec7e6b724ba
gbhakre4 <gbhakre4@gmail.com>
2018-05-29 00:05:53 +0530

    initial commit

---
+++ src/main/java/com/gilbarco/model/JsonResponse.java
@@ -0,0 +1,24 @@
+package com.gilbarco.model;
+
+public class JsonResponse {
+	
+	String timestamp;
+	int calls;
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getCalls() {
+		return calls;
+	}
+
+	public void setCalls(int calls) {
+		this.calls = calls;
+	}
+}
+
